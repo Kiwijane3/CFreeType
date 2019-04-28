f3222893af2ae1ec62c7ada834ede525e2286d26
Jane Fraser <janef0421@gmail.com>
2019-04-29 11:32:56 +1200

    Updated package again.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:3.0.2
+// swift-tools-version:4.0.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,5 +6,5 @@ import PackageDescription
 let package = Package(
     name: "CFreeType",
     pkgConfig: "freetype2",
-    providers: [.brew("freetype2"), .apt("libfreetype6-dev")]
+    providers: [.brew(["freetype2"]), .apt(["libfreetype6-dev"])]
 )
